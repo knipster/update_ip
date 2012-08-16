c7d790551deb323122f266587f6e0be678b663b7
Knipster <github@knipster.net>
2012-08-16 10:08:29 -0400

    Implemented support configuration file ~/.update_iprc to hide freedns key Implemented output and/or logfile support

--- update_ip.sh
+++ update_ip.sh
@@ -3,6 +3,16 @@
 AFRAID_HOSTNAME="<ENTERYOURSUBDOMAINHERE>"
 ROUTER_STATUS_PAGE="http://192.168.1.1/Status_Router.asp"
 AFRAID_UPDATE_URL="http://freedns.afraid.org/dynamic/update.php?<ENTERYOURKEYHERE>" 
+ROUTER_WGET_ARGS="--timeout=2 --tries=2"
+AFRAID_UPDATE_WGET_ARGS="--timeout=2 --tries=2"
+EXTRA_OUTPUT="/dev/null"  # Replace with /dev/stderr for more verbosity
+
+#  Poor Man's Config File Technique:  
+#    Copy the Above Block of variable definitions into ~/.update_iprc and replace
+#    with your own configuration
+if [ -e ~/.update_iprc ] ; then
+    source ~/.update_iprc ; 
+fi
 
 function afraid_ip() {
     # host:  lookup hostname from dns server
@@ -11,7 +21,7 @@ function afraid_ip() {
     # cut -d ' ': use space as field delimeter
     # cut -f 4: print 4th field (1 indexed)
 
-    host $AFRAID_HOSTNAME ns1.afraid.org | tail -1 | cut -d ' ' -f 4
+    host $AFRAID_HOSTNAME ns1.afraid.org | tail -1 | tee $EXTRA_OUTPUT | cut -d ' ' -f 4 
 }
 # Command to get my linksys router's external ip
 function router_ip() {
@@ -24,14 +34,15 @@ function router_ip() {
     # sed -e:  use the following quoted string
     # sed regexp:  match all characters before a digit,subgroup(\d+.\d+.\d+.\d+), then rest of characters, replace with matched subgroup
     # head -1: print the first line of STDIN  (first ip address is the wan_ip on my router)
-    wget -q -O - $ROUTER_STATUS_PAGE | \
-	sed -n -r -e 's/.*[^0-9]([1-9][0-9]{0,2}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*/\1/p' | head -1
+    echo -n "Router Wan IP: " >$EXTRA_OUTPUT
+    wget $ROUTER_WGET_ARGS -q -O - $ROUTER_STATUS_PAGE | \
+	sed -n -r -e 's/.*[^0-9]([1-9][0-9]{0,2}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}).*/\1/p' | head -1 | tee $EXTRA_OUTPUT
 }
 
 # Back tick substitutes STDOUT of command for value
 if [ `afraid_ip` = `router_ip` ]; then 
-    echo "IPs Match" ; 
+    echo  "IPs Match" > $EXTRA_OUTPUT ; 
 else 
-    echo "IPs do not match" ; 
-    wget -q -O - $AFRAID_UPDATE_URL
+    echo "IPs do not match" >$EXTRA_OUTPUT ; 
+    wget $AFRAID_UPDATE_WGET_ARGS -q -O - $AFRAID_UPDATE_URL
 fi
